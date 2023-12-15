2a6b1719f363ecf4d6794222cac91567fff71690
DiegoGallegos16 <144467169+DiegoGallegos16@users.noreply.github.com>
2023-12-15 12:07:05 -0300

    Add files via upload

---
+++ calculadora.js
@@ -0,0 +1,7 @@
+// Funcion para agregar a input 
+function agregar(ingresado){
+    document.getElementById("ingresado").value =+ingresado;
+};
+function borrar(){
+    document.getElementById("ingresado").value = ""
+};
